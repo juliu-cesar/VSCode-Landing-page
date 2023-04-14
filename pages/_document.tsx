4ce18db511f9012ed7bd5219a167af7d2f48946a
Juliu Cesar <juliucesarcardoso@gmail.com>
2023-04-14 14:34:53 -0300

    changing styledComponents to server side redering

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,56 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="pt-br">
-      <Head />
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" />
-      <link
-        href="https://fonts.googleapis.com/css2?family=Mukta:wght@300;400;500;600&display=swap"
-        rel="stylesheet"
-      />
-      <body style={{backgroundColor: "#303030"}}>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+  render(): any {
+    return (
+      <Html lang="pt-br">
+        <Head />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Mukta:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
+        <body style={{ backgroundColor: "#303030" }}>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
